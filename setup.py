27305ba8bff965bddf8f6bbef7e5f3ba1d6a70c1
Tuomo Kareoja <tuomo.kareoja@gmail.com>
2019-12-04 13:24:34 +0100

    structure and raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An exploratory analysis of the correlates of health in major US cities. Special attention given to morphine overdoses.',
+    author='Tuomo Kareoja',
+    license='MIT',
+)
